2d8932736c1d7dc5485b048ad87de1915c4f98cd
Yuhan Yan <z5270656@ad.unsw.edu.au>
2020-09-28 04:29:25 +1000

    add 2 tests of channel_addowner

--- src/channel_test.py
+++ src/channel_test.py
@@ -1,4 +1,25 @@
 '''
     the file using for channel testing
 '''
+import pytest
+import data
+import channel
+from error import InputError 
+
+# Standard situation
+def test_channel_addowner0():
+    token = get_token()
+    channel_id = get_cid()
+    u_id = get_uid()
+
+    assert(channel.channel_addowner(token, channel_id, u_id)) is True
+
+# Channel ID is not a valid channel
+def test_channel_addowner1():
+    token = get_token()
+    channel_id = get_cid()
+    u_id = get_uid()
+
+    with pytest.raises(InputError):
+        channel.channel_addowner(token, channel_id, u_id)
 

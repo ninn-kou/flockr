332e0c4f93e461aae45df3b29c1dce7d888fb9fe
Xingyu Tan <xingyu.tan@student.unsw.edu.au>
2020-09-30 02:45:22 +1000

    channel invite fuction for normall work

--- src/channel_test.py
+++ src/channel_test.py
@@ -1,11 +1,10 @@
 # this file is using for pytest of channel.py .
-import auth
-import channel
-import channels
-import pytest
+from channel import channel_invite, channel_details, channel_messages 
+from channels import channels_create
+from auth import auth_login, auth_register, auth_logout 
 from error import InputError, AccessError
-import data
-
+from data import *
+import pytest
 
 # Xingyu TAN working on channel_test.py for channel_invite fuction
 # 29 SEP 2020
@@ -28,3 +27,33 @@ THEREFORE, TEST EVERYTHING BELOW:
 - the auth user is not in this channel.
 
 """
+
+def test_channel_invite_work():
+    '''
+    this test is using for check the fuction can work normally when no Errors bring.
+    '''
+    # create 2 users 
+    user1 = auth_register("test1@test.com","check_test","Xingyu","TAN")
+    user1 = auth_login("test1@test.com","check_test")
+    u_id1 = user1['u_id']
+    u_token1 = user1['token']
+
+    user2 = auth_register("test2@test.com","check_test","steve","TAN")
+    user2 = auth_login("test2@test.com","check_test")
+    u_id2 = user2['u_id']
+    u_token2 = user2['token']
+
+    # create channel for testing
+    channel_test = channels_create(u_token1,"channel_test",True)
+    channel_test_id = channel_test['channel_id']
+
+    # testing for channel invite fuction
+    channel_invite(u_token1,channel_test_id,u_id2)
+    channel_test_details = channel_details(u_token1,channel_test_id)
+
+
+    # Assuming we the fuction running correctly, then we do check the channel details 
+    # expecially, the member infomation
+    assert u_id1 ==  channel_test_details[['all_members'][0]['u_id']]
+    assert u_id2 ==  channel_test_details[['all_members'][1]['u_id']]
+

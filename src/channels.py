a14fa9cbee376fca6f3328e837008103d06ef864
z5237212 <z5237212@cse.unsw.edu.au>
2020-09-30 20:26:54 +1000

    need the token function

--- src/channels.py
+++ src/channels.py
@@ -1,41 +1,52 @@
-#import data
+import data
+import random
+from error import InputError
+
 def channels_list(token):
     #get the u_id of the authorised user
-    global user
-    global channels
-    i = 0
-    user_id = 0
-    for i in range(len(user)):
-        if user[i]['token'] == token:
-            user_id = user[i]['u_id']
-            break
+    
+    user_id = 
+    
             
     #make a loop to check each channels       
     i = 0
     channel_list = []
-    for i in range(len(channels)):
+    for i in range(len(data.channels)):
         j = 0
         #check if the user in this channel
-        for j in range(len(channels[i]['all_members'])):
-            if channels[i]['all_members'][j]['u_id'] == user_id:        
-                channel_list.append(channels[i])
+        for j in range(len(data.channels[i]['all_members'])):
+            if data.channels[i]['all_members'][j]['u_id'] == user_id:        
+                channel_list.append(data.channels[i])
         
     return channel_list
 def channels_listall(token):
 
     #just return all channels? sure about that?
-    return channels
+    return data.channels
+    
+def create_channel_id(channels):
+    # create a random 32 bit unsigned integer
+    channel_id = random.randint(0, 0xFFFFFFFF)
+
+    # a recursive function to check whether the channel_id is unique
+    for channel in channels:
+        if channel['channel_id'] == channel_id:
+            channel_id = create_channel_id(channel_id, channels)
+            break
+    
+    return channel_id
+    
 def channels_create(token, name, is_public):
 
     if len(name) > 20:
-        print("name is too lone")
-        exit(1)
+        raise InputError
+    return
      
     owner_id = 
-    owner_FN =
+    owner_FN = 
     owner_LN = 
         
-    channel_id = len(channels) + 1
+    channel_id = create_channel_id(data.channels)
     channel_new = {}
     channel_new['channel_id'] = channel_id
     channel_new['name'] = name
@@ -46,7 +57,7 @@ def channels_create(token, name, is_public):
     channel_new['all_members']['u_id'] = owner_id
     channel_new['all_members']['FirstN'] = owner_FN
     channel_new['all_members']['LastN'] = owner_LN
-    channels.append(channel_new)
+    data.append_channels(channel_new)
     
     return channel_id
     

c6998e9396cd112c96a11846c16abdda4e1f2329
z5237212 <z5237212@cse.unsw.edu.au>
2020-10-01 21:38:22 +1000

    pass the tests

--- src/channels.py
+++ src/channels.py
@@ -5,12 +5,6 @@ import auth
 from other import clear
 
 def channels_list(token):
-    #initialise the channels list
-    data.init_channels()
-    
-    # initialise the users list
-    data.init_users()
-    
     #get the u_id of the authorised user
     for i in data.users:
         if i['token'] == token:
@@ -48,20 +42,18 @@ def create_channel_id(channels):
 def channels_create(token, name, is_public):    
     #initialise the channels list
     data.init_channels()
-    # initialise the users list
-    data.init_users()
     
     if len(name) > 20:
         raise InputError
         return
-    
+    #find the detail of the user by token
     for i in data.users:
         if i['token'] == token:
             owner_id = i['u_id']
             owner_FN = i['name_first']
             owner_LN = i['name_last']
             break    
-    
+    #make a random channel id
     channel_id = create_channel_id(data.channels)
     channel_new = {
         'name': name,
@@ -82,77 +74,8 @@ def channels_create(token, name, is_public):
         ],   
         'is_public': is_public,
     }
-    
+    #add it to the data
     data.append_channels(channel_new)
     
 
     return channel_id
-'''
-clear()
-#initialise the channels list
-data.init_channels()
-# initialise the users list
-data.init_users()
-#create two user and take their id and token
-user1 = auth.auth_register('1234@test.com', 'password', 'FirstN', 'LastN')
-user1 = auth.auth_login('1234@test.com', 'password')
-u1_id = user1['u_id']
-u1_token = user1['token']
-
-user2 = auth.auth_register('2345@test.com', 'password', 'FirstN2', 'LastN2')
-user2 = auth.auth_login('2345@test.com', 'password')
-u2_id = user2['u_id']
-u2_token = user2['token']
-
-#create a channel by user1 in channels and return its channel id
-channel_1_id = channels_create(u1_token,'team',True)
-
-#create a channel by user2 in channels and return its channel id
-channel_2_id = channels_create(u2_token,'team2',True)
-
-print(data.channels)
-
-channel_listall = [
-        {
-            'name':'team',
-            'channel_id':channel_1_id,
-            'owner':[
-                {
-                    'u_id': u1_id,
-                    'name_first': 'FirstN',
-                    'name_last': 'LastN'
-                }
-            ],
-            'all_members':[
-                {
-                    'u_id': u1_id,
-                    'name_first': 'FirstN',
-                    'name_last': 'LastN'
-                }
-            ],
-            'is_public':True
-        } , {
-            'name':'team2',
-            'channel_id':channel_2_id,
-            'owner':[
-                {
-                    'u_id': u2_id,
-                    'name_first': 'FirstN2',
-                    'name_last': 'LastN2'
-                }
-            ],
-            'all_members':[
-                {
-                    'u_id': u2_id,
-                    'name_first': 'FirstN2',
-                    'name_last': 'LastN2'
-                }
-            ],            
-            'is_public':True
-        }
-    ]
-print(channel_listall)
-print(channels_listall(u1_token))
-if channel_listall == channels_listall(u1_token):
-    print("yes")
-'''

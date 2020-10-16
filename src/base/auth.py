962c7372a460e288b8d42b28f4502d0c9d94e048
z5258819 <joseph.jeong@student.unsw.edu.au>
2020-10-16 15:24:18 +1100

    fixed commenting in auth.py

--- src/base/auth.py
+++ src/base/auth.py
@@ -19,6 +19,7 @@ def regex_email_check(email):
 
 def check_in_users(data_type, users, item):
     """Check for a particular data type in users list."""
+
     focus_user = None
     for user in users:
         if user[data_type] == item:
@@ -31,9 +32,13 @@ def create_token(u_id, users):
     """Create a 20 character long ascii string for token."""
     # Create list of random characters and length of token.
     valid_characters = string.ascii_letters + string.digits + string.punctuation
-    token_length = 20                       # Create a random token.
+    token_length = 20
+
+    # create token of that length and with specified characters
     token = "".join(random.choices(valid_characters, k = token_length))
-    for user in users:                      # Check that token is unique.
+
+    # check if token is already being used
+    for user in users:
         if user['token'] == token:
             token = create_token(u_id, users)
             break
@@ -43,14 +48,17 @@ def create_token(u_id, users):
 
 def create_u_id(users):
     """Create a random 32 bit unsigned integer to use as a u_id."""
-    u_id = random.randint(0, 0xFFFFFFFF)    # Create a random 32 bit unsigned integer.
+
+    # Create a random 32 bit unsigned integer.
+    u_id = random.randint(0, 0xFFFFFFFF)
+
     # Simple recursive function to check whether u_id is unique.
     for user in users:
         if user['u_id'] == u_id:
             u_id = create_u_id(users)
             break
-    return u_id
 
+    return u_id
 
 def handle_variabliser(handle, variabliser_num, variabliser, users):
     """
@@ -85,41 +93,59 @@ def handle_variabliser(handle, variabliser_num, variabliser, users):
 
 def handle_generator(name_first, name_last, users):
     """Generates a unique handle."""
+
+    # Create base concatenation.
     raw_concatenation = name_first + name_last
-    if len(raw_concatenation) > 20:         # Create base concatenation.
+
+    if len(raw_concatenation) > 20:
         raw_concatenation = raw_concatenation[:20]
+    # make sure handle is unique
     handle = handle_variabliser(raw_concatenation, 0, '', users)
-    return handle                           # Create a unique handle.
+
+    return handle
 
 
 def auth_register_error_check(email, password, name_first, name_last):
     """Handles error checking for auth_register."""
-    data.init_users()                       # Initialise user data.
-    regex_email_check(email)                # Check for valid input.
+    # Initialise user data.
+    data.init_users()
 
+    # Check for valid input.
+    regex_email_check(email)
+
+    # Check if email is already used.
     if check_in_users('email', data.users, email) is not None:
-        raise InputError('1')                  # Check if email is already used.
+        raise InputError('1')
 
+    # check len(password) >= 6.
     if len(password) < 6:
-        raise InputError('1')                  # check len(password) >= 6.
+        raise InputError('1')
 
+    # Check first name matches requirements.
     if len(name_first) < 1 or len(name_first) > 50:
-        raise InputError('1')                  # Check first name matches requirements.
+        raise InputError('1')
 
+    # Check Last Name matches requirements.
     if len(name_last) < 1 or len(name_last) > 50:
-        raise InputError('1')                  # Check Last Name matches requirements.
-
+        raise InputError('1')
 
 def auth_register(email, password, name_first, name_last):
     """ Function to register a new user to the program."""
 
+    # check for errors in input
     auth_register_error_check(email, password, name_first, name_last)
 
-    u_id = create_u_id(data.users)          # Create a unique u_id.
-    token = create_token(u_id, data.users)  # creates a random and unique token.
+    # Create a unique u_id.
+    u_id = create_u_id(data.users)
+
+    # creates a random and unique token.
+    token = create_token(u_id, data.users)
+
+    # create a unique handle
     handle = handle_generator(name_first, name_last, data.users)
 
-    user = {                                # Create and store a user object.
+    # Create and store a user object.
+    user = {
         'u_id': u_id,
         'email': email,
         'name_first': name_first,
@@ -130,7 +156,8 @@ def auth_register(email, password, name_first, name_last):
     }
     data.append_users(user)
 
-    token_object = {                        # Creates an object with u_id and token.
+    # Creates an object with u_id and token.
+    token_object = {
         'u_id': u_id,
         'token': token
     }
@@ -139,31 +166,44 @@ def auth_register(email, password, name_first, name_last):
 
 def auth_login(email, password):
     """ Used to log user into program."""
-    data.init_users()                       # Initialise user data.
-    regex_email_check(email)                # check if email is valid.
 
+    # Initialise user data.
+    data.init_users()
+
+    # check if email is valid.
+    regex_email_check(email)
+
+    # Check if email is used by user.
     focus_user = check_in_users('email', data.users, email)
-    if focus_user is None:                  # Check if email is used by user.
-        raise InputError                    # If not stored, raise an error.
 
+    # If not stored, raise an error.
+    if focus_user is None:
+        raise InputError
+
+    # Check password is correct
     if focus_user['password'] != password:
-        raise InputError                    # Check password.
+        raise InputError
 
-    u_id = focus_user['u_id']               # If all passed, create a token.
+    # Creates an object with u_id and token.
+    u_id = focus_user['u_id']
     token = create_token(u_id, data.users)
 
-    token_object = {                        # Creates an object with u_id and token.
+    token_object = {
         'u_id': u_id,
         'token': token
     }
-    data.add_token(token_object)            # Add token to program.
-    return token_object
 
+    # Add token to program.
+    data.add_token(token_object)
+
+    return token_object
 
 def auth_logout(token):
     """Used to log user out of program."""
-    user = data.remove_token(token)         # Search for token in token dict.
+    # Search for token in token dict.
+    user = data.remove_token(token)
 
+    # Returns accordingly if token is found.
     if user is None:
-        return {'is_success': False}        # Returns accordingly if token is found.
+        return {'is_success': False}
     return {'is_success': True}

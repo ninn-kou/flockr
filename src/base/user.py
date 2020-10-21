055bfa5b0435a0c9200e4f7c197768eb51a03af6
Yuhan Yan <z5270656@ad.unsw.edu.au>
2020-10-22 03:14:07 +1100

    finish the 4 tests of user profile

--- src/base/user.py
+++ src/base/user.py
@@ -1,13 +1,25 @@
+import data.data as data
+
+import jwt
+from   jwt import DecodeError
+
+from base.auth import JWT_SECRET,check_in_users,regex_email_check
+from base.error import InputError
+
 def user_profile(token, u_id):
-    return {
-        'user': {
-        	'u_id': 1,
-        	'email': 'cs1531@cse.unsw.edu.au',
-        	'name_first': 'Hayden',
-        	'name_last': 'Jacobs',
-        	'handle_str': 'hjacobs',
-        },
-    }
+    try:
+        email = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+    except DecodeError as e:
+        return {'is_success': False}
+    try:
+        u_id=int(u_id)
+    except Exception as e:
+        raise InputError('terrible uid')
+    user=check_in_users("u_id",data.return_users(),u_id)
+    if user:
+        return user
+    else:
+        raise InputError('not user')
 
 def user_profile_setname(token, name_first, name_last):
     return {

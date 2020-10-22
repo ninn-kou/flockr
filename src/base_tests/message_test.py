d6b493b6fdbd5fd3754d2ec908f88ae9f28749ac
Xingyu Tan <xingyu.tan@student.unsw.edu.au>
2020-10-22 21:24:01 +1100

    test if we can show the correct channel_message information

--- src/base_tests/message_test.py
+++ src/base_tests/message_test.py
@@ -191,3 +191,30 @@ def test_channel_message_newest_one_index():
     assert(check_work_msg['messages'][0]['message'] == 'the next one')
 
 ###########################################################################################
+# case 4: test if we can show the correct channel_message information
+def test_channel_message_correct_message_infors():
+    '''
+    this test using for check if the channel function can return correctly
+    2. check the function can return the message correctly.
+    2.1 the [0] always the top fresh one
+    '''
+    # create 2 users
+    other.clear()
+    user1 = auth_register("test1@test.com", "check_test", "Xingyu", "TAN")
+    user1 = auth_login("test1@test.com", "check_test")
+    u_token1 = user1['token']
+
+
+    # create channel for testing
+    channel_test_id = channels_create(u_token1,"channel_test",True)
+
+    #create test message we needed
+    message_send(u_token1, channel_test_id, "msg test 01")
+    message_send(u_token1, channel_test_id, "msg test 02")
+    message_send(u_token1, channel_test_id, "msg test 03")
+
+    # 2. check the function can return the message correctly.
+    check_work_msg = channel_messages(u_token1, channel_test_id, 0)
+    assert(check_work_msg['messages'][0]['message'] == 'msg test 03')
+    assert(check_work_msg['messages'][1]['message'] == 'msg test 02')
+    assert(check_work_msg['messages'][2]['message'] == 'msg test 01')

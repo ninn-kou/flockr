e680b86926ec7aea59a16de5034580ca313aafc7
Xingyu Tan <xingyu.tan@student.unsw.edu.au>
2020-10-22 23:42:26 +1100

    message_remove test for input error

--- src/base_tests/message_test.py
+++ src/base_tests/message_test.py
@@ -5,7 +5,6 @@ from base.channels import channels_create
 from base.auth import auth_login, auth_register, auth_logout
 from base.message import message_send, message_remove, message_edit
 from base.error import InputError, AccessError
-import data.data as data
 import base.other as other
 
 
@@ -17,28 +16,28 @@ import base.other as other
 # Xingyu TAN working on message_test.py for message_send function
 # 22 Oct. 2020
 
-"""
-    message_send()
-    Send a message from authorised_user to the channel specified by channel_id
+##########################################################################
+#    message_send()
+#    Send a message from authorised_user to the channel specified by channel_id
+#
+#    Args:
+#        token: the token of the sender.
+#        channel_id: the channel which is the target of message.
+#        message: the message we send.
+#
+#    RETURNS:
+#    { message_id }
+#
+#
+#    THEREFORE, TEST EVERYTHING BELOW:
+#    1. inputError
+#    - Message is more than 1000 characters
+#
+#    2. accessError
+#    - the authorised user has not joined the channel they are trying to post to
+#    - cannot find the channel_id
+############################################################################
 
-    Args:
-        token: the token of the sender.
-        channel_id: the channel which is the target of message.
-        message: the message we send.
-
-    RETURNS:
-    { message_id }
-
-
-    THEREFORE, TEST EVERYTHING BELOW:
-    1. inputError
-    - Message is more than 1000 characters
-
-    2. accessError
-    - the authorised user has not joined the channel they are trying to post to
-    - cannot find the channel_id
-
-"""
 ########################################################################
 #######################  test for input error  #########################
 def test_message_input_error():
@@ -91,6 +90,9 @@ def test_message_access_error_wrong_token():
     with pytest.raises(AccessError):
         message_send(u_token2, channel_test_id, message_test)
 
+    auth_logout(u_token1)
+    auth_logout(u_token2)
+
 ###########################################################################################
 def test_access_error_invalid_channelid():
     '''
@@ -118,6 +120,7 @@ def test_access_error_invalid_channelid():
     with pytest.raises(AccessError):
         message_send(u_token1, channel_test_id + 0xf, message_test)
 
+    auth_logout(u_token1)
 
 ######   test  for normally channel_messsge work and correct message_send return #########
 # case 1: return -1 : for no more message after start
@@ -138,6 +141,7 @@ def test_channel_message_return_negative_one():
     # 1. return -1 : for no more message after start
     check_return_negative_one = channel_messages(u_token1, channel_test_id, 0)
     assert check_return_negative_one['end'] == -1
+
     auth_logout(u_token1)
 ###########################################################################################
 
@@ -164,6 +168,8 @@ def test_channel_message_return50_end():
     check_work_msg = channel_messages(u_token1, channel_test_id, 0)
     #0< number && number <= 50: exist messages after start and no more than 50 messages.
     assert check_work_msg['end'] == 50
+
+    auth_logout(u_token1)
 ###########################################################################################
 
 # case 3: return 50; test for the newest one when total msg more than 50
@@ -194,6 +200,7 @@ def test_channel_message_newest_one_index():
     check_work_msg = channel_messages(u_token1, channel_test_id, 0)
     assert check_work_msg['messages'][0]['message'] == 'the next one'
 
+    auth_logout(u_token1)
 ###########################################################################################
 # case 4: test if we can show the correct channel_message information
 def test_channel_message_correct_message_infors():
@@ -222,6 +229,8 @@ def test_channel_message_correct_message_infors():
     assert check_work_msg['messages'][0]['message'] == 'msg test 03'
     assert check_work_msg['messages'][1]['message'] == 'msg test 02'
     assert check_work_msg['messages'][2]['message'] == 'msg test 01'
+
+    auth_logout(u_token1)
 ###########################################################################################
 # case 5: test if we can show the correct channel_message information
 def test_channel_message_correct_send_return_id():
@@ -245,6 +254,7 @@ def test_channel_message_correct_send_return_id():
     assert message_send(u_token1, channel_test_id, "msg test 02")['message_id'] == 2
     assert message_send(u_token1, channel_test_id, "msg test 03")['message_id'] == 3
 
+    auth_logout(u_token1)
 
 #########################################################################
 #
@@ -254,26 +264,49 @@ def test_channel_message_correct_send_return_id():
 # Xingyu TAN working on message_test.py for message_remove function
 # 22 Oct. 2020
 
-"""
-    message_remove()
-    Given a message_id for a message, this message is removed from the channel
+##########################################################################
+#    message_remove()
+#    Given a message_id for a message, this message is removed from the channel
+#
+#    Args:
+#        token: the token of the people who authority.
+#        channel_id: the channel which is the target of message.
+#
+#    RETURNS:
+#    {}
+#
+#
+#    THEREFORE, TEST EVERYTHING BELOW:
+#    1. inputError
+#    - Message id is not exist
+#
+#    2. accessError excluding
+#    - Message with message_id was sent by the authorised user making this reques
+#    - The authorised user is an owner of this channel or the flockr
+##########################################################################
 
-    Args:
-        token: the token of the people who authority.
-        channel_id: the channel which is the target of message.
-
-    RETURNS:
-    {}
-
-
-    THEREFORE, TEST EVERYTHING BELOW:
-    1. inputError
-    - Message id is not exist
-
-    2. accessError
-    - the authorised user has not joined the channel they are trying to post to
-    - cannot find the channel_id
-
-"""
-########################################################################
 #######################  test for input error  #########################
+def test_message_remoe_wrong_msg_id():
+    '''
+    this test using for check if the message id given is valid
+    '''
+    # create 2 users
+    other.clear()
+    user1 = auth_register("test1@test.com", "check_test", "Xingyu", "TAN")
+    user1 = auth_login("test1@test.com", "check_test")
+    u_token1 = user1['token']
+
+
+    # create channel for testing
+    channel_test_id = channels_create(u_token1, "channel_test", True)
+
+    #create test message we needed
+    message_send(u_token1, channel_test_id, "msg test 01")
+    message_send(u_token1, channel_test_id, "msg test 02")
+    message_test_id = message_send(u_token1, channel_test_id, "msg test 03")['message_id']
+
+    # testing for channel message function for invalid message id inputError
+    with pytest.raises(InputError):
+        message_remove(u_token1, message_test_id + 0xf)
+
+    auth_logout(u_token1)
